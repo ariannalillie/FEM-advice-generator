eca63d85f7a5881bcbdcb50a085c20a68c1b4fe6
Arianna Johnson <ariannajohnson588@gmail.com>
2022-09-20 09:39:50 -0400

    change advice text onclick

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
-import dice from './images/icon-dice.svg';
+import dice from "./images/icon-dice.svg";
 
 function App() {
   const [advice, setAdvice] = useState("");
 
+  const fetchData = async () => {
+    const data = await axios.get("https://api.adviceslip.com/advice");
+    setAdvice(data.data.slip.advice);
+  };
+
   useEffect(() => {
-    const fetchData = async () => {
-      const data = await axios.get("https://api.adviceslip.com/advice");
-      setAdvice(data.data.slip.advice);
-    };
     fetchData();
   }, []);
+
+  const HandleClick = async () => {
+    fetchData();
+  };
 
   return (
     <div className="App">
       <div className="advice-container">
         <h1 className="advice-number">Advice #117</h1>
-        <h1 className="advice-text">{advice}</h1>
-        <div className="advice-button">
+        <h1 className="advice-text">{`"${advice}"`}</h1>
+        <div className="advice-button" onClick={HandleClick}>
           <img src={dice} alt="dice" />
         </div>
       </div>
