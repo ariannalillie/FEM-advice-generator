f6cc0980c0d9e52691e3e103e344d3296d95f918
Arianna Johnson <ariannajohnson588@gmail.com>
2022-09-20 09:19:30 -0400

    fetch data from advice api

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [advice, setAdvice] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await axios.get("https://api.adviceslip.com/advice");
+      setAdvice(data.data.slip.advice);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      Advice Generator
+      <h1 className="advice">{advice}</h1>
     </div>
   );
 }
