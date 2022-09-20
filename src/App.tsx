b8d7cc4fe99b6df87d5b39d9729b5649e31d9ce7
Arianna Johnson <ariannajohnson588@gmail.com>
2022-09-20 09:34:50 -0400

    styling for desktop

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
+import dice from './images/icon-dice.svg';
 
 function App() {
   const [advice, setAdvice] = useState("");
@@ -15,7 +16,13 @@ function App() {
 
   return (
     <div className="App">
-      <h1 className="advice">{advice}</h1>
+      <div className="advice-container">
+        <h1 className="advice-number">Advice #117</h1>
+        <h1 className="advice-text">{advice}</h1>
+        <div className="advice-button">
+          <img src={dice} alt="dice" />
+        </div>
+      </div>
     </div>
   );
 }
